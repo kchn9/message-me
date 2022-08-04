b4c38566a597a1f91d8222e66c2bdc3bf781eaf8
kchn9 <mat.kuchnia@gmail.com>
2022-08-04 23:18:58 +0200

    Create user list with random avatar generation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def random_avatar_for_user(user)
+    # username = user.username;]
+    seed = user.bytes.shuffle[1..10].join
+    src = "https://avatars.dicebear.com/api/big-smile/#{seed}.svg"
+    image_tag(src, alt: user, class: "ui avatar image")
+  end
+
 end
