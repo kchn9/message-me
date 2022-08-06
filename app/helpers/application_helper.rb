ea460b8dd176ef57c43ab09832d722a818390c2c
kchn9 <mat.kuchnia@gmail.com>
2022-08-06 15:56:16 +0200

    Connect chatroom with messages table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,8 @@
 module ApplicationHelper
-  def random_avatar_for_user(user)
-    # username = user.username;]
-    seed = user.bytes.shuffle[1..10].join
-    src = "https://avatars.dicebear.com/api/big-smile/#{seed}.svg"
-    image_tag(src, alt: user, class: "ui avatar image")
+  def get_avatar_for_username(username)
+    seed = username.bytes.join
+    src = "https://avatars.dicebear.com/api/big-smile/#{seed[0, 10]}.svg"
+    image_tag(src, alt: username, class: "ui avatar image")
   end
 
 end
