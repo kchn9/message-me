c295e53cbb8870b46c861d4fa6333ee9faed7f1d
kchn9 <mat.kuchnia@gmail.com>
2022-08-06 16:51:27 +0200

    Add login auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    credentials = login_params
+    user = User.find_by(username: credentials[:username])
+    if user && user.authenticate(credentials[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+  def login_params
+    params.require(:session).permit(:username, :password)
+  end
+
 end
