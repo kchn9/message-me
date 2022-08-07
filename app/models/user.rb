94d11d42528e8a17f2d6e3002ec09e09cdd87752
kchn9 <mat.kuchnia@gmail.com>
2022-08-07 21:42:48 +0200

    Add password len validation + error field highlight

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :messages
   validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6, maximum: 25 }
   validates :terms, acceptance: true
   validates :password_confirmation, presence: true
 end
